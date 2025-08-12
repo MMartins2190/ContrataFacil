3c31c2476f4ecb15b25fab6ff2a98689723c329b
Felipe <felipebpacheco2018@gmail.com>
2025-08-12 16:34:01 -0300

    frontend

---
+++ sv-frontend/app/src/components/Header.svelte
@@ -0,0 +1,24 @@
+<header style="display: flex;">
+    <p id="">Contrata Fácil</p>
+    <div>
+        <a href="/">Início</a>
+        <a href="/">Como Funciona</a>
+        <a href="/">Vagas</a>
+        <a href="/">Sobre</a>
+        <button>Entrar</button>
+    </div>
+</header>
+
+<style>
+    header{
+        display: flex;
+        padding: 1rem;
+        justify-content: space;
+        outline: 2px solid hsl(250, 100%, 50%);
+    }
+    div{
+        margin-left: auto;
+        justify-content: space-between;
+        outline: 2px solid hsl(250, 100%, 25%);
+    }
+</style>
---
+++ sv-frontend/app/src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+    import Header from '../components/Header.svelte';
+</script>
+<Header />
+<div>
+    <h1>Encontre talentos com facilidade</h1>
+    <p>Uma plataforma inteligente de currículo com foco em filtragem e categorização para facilitar contratações.</p>
+    <button>Começar Agora</button>
+    <button>Entrar com Linkedin</button>
+</div>
+<div>
+    <p>Como Funciona em 3 passos simples</p>
+    <div style="display: flex; justify-content: space-evenly">
+        <div>
+            <i>Línkedin</i>
+            <p>Login rápido com Linkedin</p>
+        </div>
+        <div>
+            <i>Línkedin</i>
+            <p>Login rápido com Linkedin</p>
+        </div>
+        <div>
+            <i>Línkedin</i>
+            <p>Login rápido com Linkedin</p>
+        </div>
+    </div>
+</div>
