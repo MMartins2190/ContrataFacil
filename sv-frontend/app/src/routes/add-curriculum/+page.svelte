4b10351bf02d3e03b888986ee58e6f497eca525d
Felipe <felipebpacheco2018@gmail.com>
2025-09-01 16:47:41 -0300

    inicio da estrutura de curriculos

---
+++ sv-frontend/app/src/routes/add-curriculum/+page.svelte
@@ -0,0 +1,39 @@
+<div class="all-father">
+    <textarea name="cv-presentation" id="cv-presentation-textarea" cols="100" rows="100"></textarea>
+    <label>Selecionar arquivo: <input type="file" value="Imagem" accept="image/*"></label>
+    <button>Enviar</button>
+</div>
+
+<style>
+    *{
+        border: none;
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+
+    .all-father{
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        outline: 2px solid purple;
+    }
+
+    textarea{
+        height: 50%;
+        border: 3px solid blue;
+    }
+
+    label{
+        border: 3px solid blue;
+    }
+
+    button{
+        background-color: white;
+        border: 3px solid blue;
+        padding: 0.25rem 2rem;
+    }
+</style>
---
+++ sv-frontend/app/src/routes/curriculums/+page.svelte
@@ -0,0 +1,17 @@
+<div class="main">
+    <h1 class="title">Currículos</h1>
+</div>
+
+<style>
+    *{
+        border: none;
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+
+    .title{
+        margin-left: 5rem;
+    }
+</style>
