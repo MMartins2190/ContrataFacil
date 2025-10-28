2bb64dc9ba3dbac1106dc677799e674aeb3b6ba3
Felipe <felipebpacheco2018@gmail.com>
2025-10-28 11:07:59 -0300

    início empresaID

--- sv-frontend/app/src/routes/enterprise-intro/+page.js
+++ sv-frontend/app/src/routes/enterprise-intro/+page.js
@@ -1,5 +1,8 @@
 import { redirect } from "@sveltejs/kit"
 
 export async function load({ fetch }) {
-    
+    fetch("http://127.0.0.1/usuarios/")
+    .then(usuarios => {
+        return "Screw";
+    })
 }
