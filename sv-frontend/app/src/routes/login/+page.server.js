5fee26ebf8e17ddb58f13dcd0723b13fdd750e25
Felipe Pacheco <felipebpacheco2018@gmail.com>
2025-11-04 06:36:06 -0300

    cookies config teste correto

--- sv-frontend/app/src/routes/login/+page.server.js
+++ sv-frontend/app/src/routes/login/+page.server.js
@@ -1,4 +1,9 @@
 const url = "http://127.0.0.1:8000/login/";
+const cookieSettings = {
+    path: '/',
+    httpOnly: true,
+    sameSite: "lax",
+}
 
 export const actions = {
     default: async ({request, fetch, cookies}) => {
@@ -11,8 +16,8 @@ export const actions = {
         })
         .then(res => res.json())
         .then(loginResponse=>{
-            cookies.set("accessToken", loginResponse.access_token, { path: '/'});
-            cookies.set("refreshToken", loginResponse.refresh_token, { path: '/'});
+            cookies.set("accessToken", loginResponse.access_token, cookieSettings);
+            cookies.set("refreshToken", loginResponse.refresh_token, cookieSettings);
         });
         } catch (err) {
             console.warn("Erro de conexão", err);
